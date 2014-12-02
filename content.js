9a63e682c6773699f3c7e5979689eed4e93c1c17
Tif Slama <tif.slama@gmail.com>
2014-12-01 17:49:26 -0800

    jquery test works! but loads on pageload

---
+++ content.js
@@ -0,0 +1,49 @@
+$("h2").append(' Helium Test!');
+  console.log("it's working");
+
+
+// $('p').mouseenter(function(){
+// 	$(this).toggleClass('inflate');
+// 	// picks a random number between 1 and 1 and plays that 'inflate' audio file
+//   $("#inflate_" + Math.ceil(Math.random() * 1))[0].play();
+//   // $(this).css({"transform: scale(4) rotate"(getRandomInt(-10,10))});
+// });
+
+// $(this).mouseleave(function(){
+// 	$(this).toggleClass('delfate');
+// 	// picks a random number between 1 and 2 and plays that 'inflate' audio file
+// 	$("#deflate_" + Math.ceil(Math.random() * 2))[0].play();
+// });
+
+
+
+// mouseenter > enlarge with sound and start a counter
+// if the counter reaches 5 seconds then pop/delete
+
+// mouseleave > deflate with other sound count back from current counter
+
+
+
+// <style>
+// .inflate {
+// 	-ms-transform: scale(3) rotate(10deg);
+// 	-webkit-transform: scale(3) rotate(10deg);
+// 	transform: scale(3) rotate(10deg);
+// 	transition-duration: 2s
+// }
+
+// .deflate {
+// 	-ms-transform: scale(4,2);
+// 	-webkit-transform: scale(4,2);
+// 	transform: scale(4,2);
+// 	transition-duration: 2s
+// }
+
+// h1 {
+// 	font-family: helvetica;
+// }
+
+// .headline {
+// 	color: red;
+// }
+// </style>
