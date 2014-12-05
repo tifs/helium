aaa3510b439f45912d17d0ba9ad548e95dff8f23
Tif Slama <tif.slama@gmail.com>
2014-12-05 10:58:04 -0800

    random audio player works, just need to record some audio now

--- content.js
+++ content.js
@@ -1,40 +1,18 @@
 $(document).ready(function(){
-
-$("body").prepend("<audio id='inflating_1' preload='auto' src='http://www.tonycuffe.com/mp3/pipers%20hut.mp3'</audio>");
-$("body").prepend("<audio id='deflating_1' preload='auto' src='http://www.tonycuffe.com/mp3/tail%20toddle.mp3'</audio>");
-  var inflateTune = document.getElementById('inflating_1');
-  var deflateTune = document.getElementById('deflating_1');
-
-  // console.dir(x)
-  // var audioEl = document.createElement('audio');
-  // audioEl.setAttribute("preload", "auto");
-  // audioEl.setAttribute ("src", "/Users/tifslama/Code/week11/heliuminflating_1.mp3");
-  // audioEl.setAttribute("id", "inflating_1");
-
-  // var deflateEl = document.createElement('audio');
-  // deflateEl.setAttribute("preload", "auto");
-  // deflateEl.setAttribute("src", "chrome-extension://heliumdeflating_1.mp3");
-  // deflateEl.setAttribute("id", "deflating_1");
-
-  // var inflateElement = document.createElement('audio');
-  //   inflateElement.setAttribute("preload", "auto");
-  //   inflateElement.setAttribute("id", "inflating_1");
-  //   inflateElement.autobuffer = true;
-
-  // var inflateSource = document.createElement('source');
-  //   inflateSource.type= 'audio/mpeg';
-  //   inflateSource.src= '/Users/tifslama/Code/week11/helium/inflating_1.mp3';
-  //   inflateElement.appendChild(inflateSource);
-
-  //  chrome.extension.onMessage.addListener(
-  //   function(request, sender, sendResponse) {
-  //     if (request.action == "play"){
-  //         inflateElement.load();
-  //         inflateElement.play();
-  //         }
-  //   });
+  // inflating audio files
+  $("body").prepend("<audio id='inflating_1' preload='auto' src='http://www.tonycuffe.com/mp3/pipers%20hut.mp3'</audio>");
+  $("body").prepend("<audio id='inflating_2' preload='auto' src='http://www.tonycuffe.com/mp3/pipers.mp3'</audio>");
+  $("body").prepend("<audio id='inflating_3' preload='auto' src='http://www.tonycuffe.com/mp3/pipers.mp3'</audio>");
+  $("body").prepend("<audio id='inflating_4' preload='auto' src='http://www.tonycuffe.com/mp3/pipers.mp3'</audio>");
+  // deflating audio files
+  $("body").prepend("<audio id='deflating_1' preload='auto' src='http://www.tonycuffe.com/mp3/tail%20toddle.mp3'</audio>");
+  $("body").prepend("<audio id='deflating_2' preload='auto' src='http://www.tonycuffe.com/mp3/tail%20.mp3'</audio>");
+  $("body").prepend("<audio id='deflating_3' preload='auto' src='http://www.tonycuffe.com/mp3/tail%20.mp3'</audio>");
+  $("body").prepend("<audio id='deflating_4' preload='auto' src='http://www.tonycuffe.com/mp3/tail%20.mp3'</audio>");
 
 
+  // var randomInflate = document.getElementById('inflating_'+ Math.ceil(Math.random()*4));
+  // var randomDeflate = document.getElementById('deflating_'+ Math.ceil(Math.random()*4));
   var remove;
 
   // mouseenter any element and run this function
@@ -46,23 +24,25 @@ $("body").prepend("<audio id='deflating_1' preload='auto' src='http://www.tonycu
       // console.dir($("#inflating_1"));
       // $("#inflating_1").play(); // this is showing up as undefined
       _this = $(this);
-
+      var inflateTune = document.getElementById('inflating_'+ Math.ceil(Math.random()*4));
+      var deflateTune = document.getElementById('deflating_'+ Math.ceil(Math.random()*4));
       inflateTune.play();
       deflateTune.pause();
       deflateTune.currentTime = 0;
-      // this isn't being called
       remove = setTimeout(function(){
         _this.remove();
         inflateTune.pause();
         inflateTune.currentTime = 0;
+        // popTune.play();
       }, 2000);
-      console.log("inflate! 2");
+      console.log(inflateTune);
     }
   });
 
 
   $("h1,h2,h3,h4,h5,h6,p,blockquote,a,li,figure,figcaption,img,button").on("mouseleave", function(){
-
+    var inflateTune = document.getElementById('inflating_'+ Math.ceil(Math.random()*4));
+    var deflateTune = document.getElementById('deflating_'+ Math.ceil(Math.random()*4));
     inflateTune.pause();
     inflateTune.currentTime = 0;
     deflateTune.play();
@@ -88,12 +68,4 @@ $("body").prepend("<audio id='deflating_1' preload='auto' src='http://www.tonycu
 //  -webkit-transform: scale(3) rotate(10deg);
 //  transition-duration: 5s
 // }
-
-// .deflate {
-//  -ms-transform: scale(4,2);
-//  -webkit-transform: scale(4,2);
-//  transform: scale(4,2);
-//  transition-duration: 2s
-// }
-
 // </style>
