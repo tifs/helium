94269359a9306f924d4215e2c4f806f511c00845
Tif Slama <tif.slama@gmail.com>
2014-12-08 19:16:13 -0800

    on/off toggle works visually, but does not stop the js. must run another js file to clear it

--- background.js
+++ background.js
@@ -1,4 +1,15 @@
+var toggle = false;
+
 chrome.browserAction.onClicked.addListener(function(activeTab) {
-    chrome.tabs.executeScript(null, {file: "jquery-1.11.1.min.js"});
-    chrome.tabs.executeScript(null, {file: "content.js"});
+  // chrome.browserAction.setIcon(object details, function callback)
+  toggle = !toggle;
+    if(toggle){
+      chrome.browserAction.setIcon({path: "helium_32_on.png", tabId:activeTab.id});
+      chrome.tabs.executeScript(null, {file: "jquery-1.11.1.min.js"});
+      chrome.tabs.executeScript(null, {file: "content.js"});
+    }
+    else{
+      chrome.browserAction.setIcon({path: "helium_32_off.png", tabId:activeTab.id});
+      chrome.tabs.executeScript(null, {file: "clear.js"});
+    }
 });
