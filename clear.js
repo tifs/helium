f9668ae9990ce186944b69c5378b15ffbf559937
Tif Slama <tif.slama@gmail.com>
2017-01-17 14:08:43 -0800

    removing js from dom isn't working the way i want...sigh

--- clear.js
+++ clear.js
@@ -1,6 +1,8 @@
 // resets all js and jquery on the page when the button's turned off
 $(document).ready(function(){
   $("audio").remove();
+  // supposed to reset the default behavior on all targeted elements, but doesn't work
+  // $("h1,h2,h3,h4,h5,h6,p,a,li,video,img,button,figcaption").unbind("click");
   $("h1,h2,h3,h4,h5,h6,p,a,li,video,img,button,figcaption").off("mouseenter");
   clearTimeout(remove);
   $("h1,h2,h3,h4,h5,h6,p,a,li,video,img,button,figcaption").off("mouseleave");
